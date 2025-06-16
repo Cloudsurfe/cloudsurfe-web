8685656ad93ea2859cdbcbe07c26bdbd86ff7a37
navneet <navneetpal.dev@gmail.com>
2025-06-16 18:31:25 +0530

    Initial commit: Setup Kotlin Multiplatform project with wasmJs target

---
+++ composeApp/src/wasmJsMain/kotlin/com/cloudsurfe/cloudsurfeweb/Platform.kt
@@ -0,0 +1,7 @@
+package com.cloudsurfe.cloudsurfeweb
+
+class WasmPlatform {
+    val name: String = "Web with Kotlin/Wasm"
+}
+
+fun getPlatform() = WasmPlatform()
---
+++ composeApp/src/wasmJsMain/kotlin/com/cloudsurfe/cloudsurfeweb/main.kt
@@ -0,0 +1,12 @@
+package com.cloudsurfe.cloudsurfeweb
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport(document.body!!) {
+        App()
+    }
+}
